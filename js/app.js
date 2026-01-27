e16134c9c9c23d3f583855d68456563f3baa1344
Miguel Alexander Domínguez Vázquez <140833902+miguelalexander241298@users.noreply.github.com>
2026-01-28 00:07:28 +0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 const hamburguer = document.getElementById('hamburguer')
+const hamburguer_obj = document.getElementById('hamburguer_obj')
+const objetivos = document.getElementById('objetivos')
+const cerrar_obj = document.querySelectorAll('.cerrar_obj')[0]
 const navbar = document.getElementById('navbar')
 const nav = document.querySelectorAll('.nav')
 var anim_navbar = false
+var anim_obj = false
 
 nav.forEach(e => {
     e.addEventListener('click', () => {
@@ -18,4 +22,19 @@ hamburguer.addEventListener('click', () => {
         navbar.style.right = '-50%';
         anim_navbar = false
     }
+})
+
+hamburguer_obj.addEventListener('click', () => {
+    if (anim_obj === false) {
+        objetivos.style.right = '0px';
+        anim_obj = true
+    } else {
+        objetivos.style.right = '-100%';
+        anim_obj = false
+    }
+})
+
+cerrar_obj.addEventListener('click', () => {
+    objetivos.style.right = '-100%';
+    anim_obj = false
 })
